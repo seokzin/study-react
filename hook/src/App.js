5b8714d061b390eadaf6a5c7da7b869045ca4f76
seokzin <seokzin95@gmail.com>
2021-03-28 19:18:27 +0900

    6. Add component remove btn

--- hook/src/App.js
+++ hook/src/App.js
@@ -2,11 +2,28 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
+  var [funcShow, setFuncShow] = useState(true);
+  var [classShow, setClassShow] = useState(true);
+
   return (
     <div className="container">
       <h1>Hello World</h1>
-      <FuncComp initNumber={2}></FuncComp>
-      <ClassComp initNumber={2}></ClassComp>
+      <input
+        type="button"
+        value="remove func"
+        onClick={function () {
+          setFuncShow(false);
+        }}
+      />
+      <input
+        type="button"
+        value="remove class"
+        onClick={function () {
+          setClassShow(false);
+        }}
+      />
+      {funcShow ? <FuncComp initNumber={2}></FuncComp> : null}
+      {classShow ? <ClassComp initNumber={2}></ClassComp> : null}
     </div>
   );
 }
@@ -128,6 +145,10 @@ class ClassComp extends React.Component {
 
   componentDidUpdate(nextProps, nextState) {
     console.log("%cclass => componentDidUpdate", classStyle);
+  }
+
+  componentWillUnmount() {
+    console.log("%cclass => componentWillUnMount", classStyle);
   }
 
   render() {
