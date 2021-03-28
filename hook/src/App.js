2d20aba8daad0cd2c42fec829f668127b30684f1
seokzin <seokzin95@gmail.com>
2021-03-28 19:10:29 +0900

    5. Lifecycle (function component)

--- hook/src/App.js
+++ hook/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
@@ -11,6 +11,9 @@ function App() {
   );
 }
 
+var funcStyle = "color: blue";
+var funcId = 0;
+
 function FuncComp(props) {
   var numberState = useState(props.initNumber);
   var number = numberState[0];
@@ -21,6 +24,60 @@ function FuncComp(props) {
   // var setDate = dateState[1];
   // 위 3줄을 한줄로 축약한 것
   var [_date, setDate] = useState(new Date().toString());
+
+  // side effect - React 컴포넌트가 화면에 렌더링된 이후에 비동기로 처리되어야 하는 부수적인 효과들
+  useEffect(
+    function () {
+      // 문법의 변화가 있었나? Id 카운트가 1, 3, 4, 6 으로 한 칸씩 뛰어간다.
+      console.log(
+        "%cfunc => useEffect number (componentDidMout & componentDidUpdate)" +
+          ++funcId,
+        funcStyle
+      );
+      document.title = number;
+      return function () {
+        // clean up - 정리하는 작업을 필요로 한다면 return 값으로 함수를 준다. 만약 useEffect가 다시 실행되면 이 함수가 콜백 함수보다 먼저 실행된다.
+        console.log(
+          "%cfunc => useEffect number return (componentDidMout & componentDidUpdate)" +
+            ++funcId,
+          funcStyle
+        );
+      };
+    },
+    [number] // skipping effect - 값이 바뀔 때만 콜백함수가 호출 된다.
+  );
+
+  useEffect(function () {
+    console.log("%cfunc => useEffect (componentDidMout)" + ++funcId, funcStyle);
+    document.title = number;
+    return function () {
+      console.log(
+        "%cfunc => useEffect return (componentWillUnMount)" + ++funcId,
+        funcStyle
+      );
+    };
+  }, []); // 최추 1회만 실행. 이후론 실행 X
+
+  useEffect(
+    function () {
+      console.log(
+        "%cfunc => useEffect date (componentDidMout & componentDidUpdate)" +
+          ++funcId,
+        funcStyle
+      );
+      document.title = number;
+      return function () {
+        console.log(
+          "%cfunc => useEffect _date return (componentDidMout & componentDidUpdate)" +
+            ++funcId,
+          funcStyle
+        );
+      };
+    },
+    [_date]
+  );
+
+  console.log("%cfunc => render " + ++funcId, funcStyle);
 
   return (
     <div className="container">
